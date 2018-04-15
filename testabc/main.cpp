0233bb714a9b90fbd1c57fb7227507d1868263e4
Serena Chan <lok.chan@kcl.ac.uk>
2018-04-15 14:54:59 +0100

    Initial Commit

---
+++ testabc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testabc
+//
+//  Created by Serena on 15/4/2018.
+//  Copyright © 2018 Serena. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
